fb8a538a0e5731763ddfdeae9a6055975fa1bea3
Bartuz <filip@bartuzi.me>
2014-05-24 11:32:49 +0200

    OrderItem model defined

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :number, type: Integer
   has_many :order_items
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,10 @@
 class OrderItem
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :quantity, type: Integer
+
   belongs_to :order
+  has_many :positions
+  
 end
