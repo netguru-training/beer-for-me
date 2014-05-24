0cdc5ed5b6e4a7dddd586500fda36b280df11f89
Bartuz <filip@bartuzi.me>
2014-05-24 13:49:10 +0200

    Order's number cycle logic

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,5 +5,21 @@ describe Order do
   it { should have_field(:number) }
   it { should have_field(:status) }
 
-  let(:order) { create(:order) }
+  let!(:order) { create(:order) }
+  let!(:order2) { create(:order) }
+  
+  it "should set different orders number" do
+    expect(order.number).not_to equal(order2.number)
+  end
+
+  it "should increase newer order number by one" do
+    expect(order2.number).to eq(order.number+1)
+  end
+
+  it "should set reset number cycle" do
+    order.update_attribute(:number, 60)
+    order3 = FactoryGirl.create(:order)
+    expect(order3.number).to eq(10)
+  end
+
 end
