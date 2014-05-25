38761f6f03650a94e240c6bde9eb4fe3f984666b
saasbook <saasbook@saasbook.(none)>
2014-05-25 09:07:11 -0300

    prevent form rendering layout when ajax request

--- spec/controllers/waiter/waiter_controller_spec.rb
+++ spec/controllers/waiter/waiter_controller_spec.rb
@@ -5,7 +5,7 @@ describe Waiter::OrdersController do
   describe "New order" do
     let(:position) { create(:position) }
 
-    it "exposes menu items" do
+    it "exposes menu positions" do
       controller.positions.should include position
     end
 
