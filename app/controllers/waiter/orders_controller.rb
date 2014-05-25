f808351b373e7d900ce462224d76bc1267960517
saasbook <saasbook@saasbook.(none)>
2014-05-25 08:40:13 -0300

    fixed bug where you could add empty orders

--- app/controllers/waiter/orders_controller.rb
+++ app/controllers/waiter/orders_controller.rb
@@ -9,15 +9,24 @@ module Waiter
     end
 
     def create
+      # binding.pry
+
       order = Order.create
       order_items = params[:order_items]
 
       order_items.each do |oi|
+        quantity = oi['quantity']
         position = Position.find(oi['id'])
-        order.order_items.create(quantity: oi['quantity'], position: position)
+        
+        order.order_items.create(quantity: quantity, position: position) if  quantity.to_i > 0
       end
 
-      flash[:notice] = "Waiter created order number #{order.number}"
+      flash[:notice] = if order.order_items.empty?
+                        order.destroy
+                        "You cannot add empty order"
+                      else
+                        "Waiter created order number #{order.number}"
+                      end
 
       redirect_to new_waiter_order_path
     end
