ac675277bf45df0392a5f87b875c3177b567df90
Bartuz <filip@bartuzi.me>
2014-05-25 11:54:00 +0200

    Moves button logic to decorator

--- app/decorators/chef/order_decorator.rb
+++ app/decorators/chef/order_decorator.rb
@@ -1,6 +1,10 @@
 class Chef::OrderDecorator < Draper::Decorator
   delegate_all
 
+  def show_status_change_button?
+    true if self.status == 'PENDING'
+  end
+
   def hide_items?
     false
   end
--- app/decorators/dashboard/order_decorator.rb
+++ app/decorators/dashboard/order_decorator.rb
@@ -1,6 +1,10 @@
 class Dashboard::OrderDecorator < Draper::Decorator
   delegate_all
   
+   def show_status_change_button?
+    true if self.status == 'READY'
+  end
+
   def hide_items?
     true
   end
