79c1e8fe51ca870877ca927eeca2a78977038afa
Michał Poczwardowski <dmp0x7c5@gmail.com>
2014-05-25 14:15:26 +0200

    Add jQuery dashboard on call replace

--- app/assets/javascripts/reload_dashboard.js
+++ app/assets/javascripts/reload_dashboard.js
@@ -1,11 +1,17 @@
-
 var time = 5000;
 
-$(function() {
-    if ($('body.dashboard_orders_controller.index').length )
-    {
+$(function () {
+    if ($('body.dashboard_orders_controller.index').length) {
         setInterval(function () {
-            window.location.reload();
+            //window.location.reload();
+            var jqxhr = $.get("")
+                .done(function ( data ) {
+                    $('#js_content').html(data)
+                })
+                .fail(function () {
+                    alert("error");
+                })
+
         }, time);
     }
 });
